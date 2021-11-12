3602359f31a501fde1f4eb18aab5af1618345601
Joanna Mackiewicz <macjoannna@gmail.com>
2021-11-13 00:50:47 +0100

    test_id_generator added

--- tests/test_id_generator.py
+++ tests/test_id_generator.py
@@ -1,6 +1,11 @@
+import numpy as np
+import pandas as pd
+
 from cc_detector.ids_generator import players_id_df
 
 def test_players_id_df():
-    res = players_id_df()
-    assert res.shape() == (2,)
-    
+    df_dummy = pd.DataFrame({'White' : ["12345", 'DummyName', "1234", "forlat", "Geforce"],
+    "Black" : ['DummyName', "12345", "Dummy", "Geforce", "Bambi"]})
+    players_id = pd.DataFrame({'Players': [], 'Player_ID' : []})
+    players_id_df(df_dummy, players_id)
+    assert len(players_id.index) == len(np.unique(players_id))
