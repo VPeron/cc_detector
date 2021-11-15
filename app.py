1b258c84f106393227c31eaef15724bcebb91174
Vinicius Peron <viperon.python@gmail.com>
2021-11-15 22:29:43 +0100

    implemented frontend with access to all dfs via API

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from cc_detector.data import ChessData
+import matplotlib.pyplot as plt
+import requests
+
+
+# HEADER
+st.write('## Chess Files')
+img = "https://images3.alphacoders.com/235/235755.jpg"
+st.image(img)
+
+# DATA
+@st.cache
+def get_data():
+    url_ep = 'http://127.0.0.1:8000/predict'
+    res = requests.get(url_ep)
+    result = res.json()
+    df_players = pd.DataFrame(result['players'])
+    df_games = pd.DataFrame(result['games'])
+    # df_moves = pd.DataFrame(result['moves'])
+    return df_players, df_games #, df_moves
+
+df_players, df_games = get_data()
+
+# st.write(df_players)
+# st.write(df_games)
+
+def player_search():
+    title = st.text_input('Search Player games', '')
+    if title:
+        white = df_games[df_games['White'] == title]
+        black = df_games[df_games['Black'] == title]
+        player = white.append(black)
+        st.write(player)
+        
+        if len(player) > 1:
+            # PLOT
+            fig, ax = plt.subplots()
+            player['Date'] = pd.to_datetime(player['Date'])
+            player.sort_values(by='Date', inplace=True)
+            plt.title('Rating')
+            plt.plot(player['White_Elo'])
+            plt.grid()
+            st.pyplot(fig)
+player_search()
+
+#TODO search opening
+#TODO view board img
+#TODO plot generic game or player overview 
+    
+
+def sidebar():
+    # sidebar title
+    st.sidebar.write('## Files sidebar')
+    # 1st dropdown
+    add_selectbox = st.sidebar.selectbox(
+        "Select list view.",
+        ("players", "games")
+    )
+    if add_selectbox == 'players':
+        white = df_players['White'].unique()
+        black = df_players['Black'].unique()
+        players = np.concatenate((white, black))
+        # players = white.append(black)
+        st.sidebar.write(players)
+        
+    if add_selectbox == 'games':
+        st.sidebar.write(df_games)
+sidebar()
+
+
+
--- chessapi.py
+++ chessapi.py
@@ -22,18 +22,26 @@ def index():
 
 
 @app.get("/predict")
-def test_epoint(df):
+def test_epoint():
     chessdata = ChessData()
     player_df, game_df, move_df = chessdata.import_data(data_path='raw_data/Fics_data_pc_data.pgn', 
                                                               import_lim=50)
-    if df == 'players':
-        players = player_df['White'].unique()
-        return  {'players': list(players)}
-    if df == 'games':
-        games = game_df['Game_ID']
-        return  {'games': list(games)}
-    if df == 'moves':
-        moves = [move for move in move_df['FEN_moves']]
-        return  {'players': moves}
-    else:
-        return{'result': 'No results'}
+    # if df == 'players':
+    #     players = player_df['White'].unique()
+    #     return  {'players': list(players)}
+    # if df == 'games':
+    #     games = game_df['Game_ID']
+    #     return  {'games': list(games)}
+    # if df == 'moves':
+    #     moves = [move for move in move_df['FEN_moves']]
+    #     return  {'players': moves}
+    # else:
+    #     return{'result': 'No results'}
+    players = player_df.to_dict()
+    games = game_df.to_dict()
+    moves = move_df.to_dict()
+    return {
+        'players':players,
+        'games':games,
+        'moves':moves
+    }
